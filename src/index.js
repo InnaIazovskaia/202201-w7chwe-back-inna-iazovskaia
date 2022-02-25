e582b93880d9ae5edc2586f27eec16e9f413b1ee
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-25 19:39:19 +0100

    created index.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const debug = require("debug")("socialnet:root");
+const chalk = require("chalk");
+const uppServer = require("./server/uppServer");
+const app = require("./server/index");
+
+const port = process.env.PORT || 4000;
+
+(async () => {
+  try {
+    await uppServer(port, app);
+  } catch (error) {
+    debug(chalk.redBright(`Error: ${error.message}`));
+  }
+})();

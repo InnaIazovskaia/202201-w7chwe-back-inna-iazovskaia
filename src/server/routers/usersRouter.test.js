cf8fa6011777c892d923edd3d0d3256a77bd22b0
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-28 18:49:01 +0100

    added test for /login endpoint in usersRouter.test.js

--- src/server/routers/usersRouter.test.js
+++ src/server/routers/usersRouter.test.js
@@ -69,3 +69,59 @@ describe("Given a /users/register/ endpoint", () => {
     });
   });
 });
+
+describe("Given a /users/login/ endpoint", () => {
+  describe("When it receives a POST request with valid username and password", () => {
+    test("Then it should respond with 200 status code and token", async () => {
+      const user = {
+        name: "Tomas",
+        username: "superTomas",
+        password: "userpassword",
+      };
+      const { body } = await request(app)
+        .post("/users/login")
+        .send(user)
+        .expect(200);
+
+      expect(body).toHaveProperty("token");
+    });
+  });
+
+  describe("When it receives a POST request with valid username", () => {
+    test("Then it should respond with 401 status code error message 'User not found'", async () => {
+      const user = {
+        name: "Testman",
+        username: "Testman",
+        password: "testpassa",
+      };
+      const expectedErrorMessage = "User not found";
+
+      const { body } = await request(app)
+        .post("/users/login")
+        .send(user)
+        .expect(401);
+
+      expect(body).toHaveProperty("error");
+      expect(body.message).toBe(expectedErrorMessage);
+    });
+  });
+
+  describe("When it receives a POST request with valid username 'superTomas' and invalid password", () => {
+    test("Then it should respond with 401 code and error message 'Invalid password for user superTomas'", async () => {
+      const user = {
+        name: "Tomas",
+        username: "superTomas",
+        password: "lsadfklkdf",
+      };
+      const expectedErrorMessage = "Invalid password for user superTomas";
+
+      const { body } = await request(app)
+        .post("/users/login")
+        .send(user)
+        .expect(401);
+
+      expect(body).toHaveProperty("error");
+      expect(body.message).toBe(expectedErrorMessage);
+    });
+  });
+});
